511a422024ff4ee645c666a52dcd21b6b41f4ec0
Elijah Molloy <elijahmolloy@gmail.com>
2023-04-23 01:38:37 -0600

    Added swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableVersioning({
 		type: VersioningType.URI
 	});
-	
+
 	app.useGlobalPipes(new ValidationPipe({
 		whitelist: true,
 		forbidNonWhitelisted: true,
 		transform: true
 	}));
+
+	const config = new DocumentBuilder()
+		.setTitle('Bye Book API')
+		.setDescription('Beta-version of Bye Book API')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
 	
 	await app.listen(3000);
 }
