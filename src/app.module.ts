d49dea1ac498c2b1411f5f7b7750b90ae1da84d5
Elijah Molloy <elijahmolloy@gmail.com>
2023-04-23 01:22:40 -0600

    Addition of mongoose. Update of prettierrc spacing. yarn lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [],
-  providers: [],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot(process.env.MONGODB_URL)
+	],
+	controllers: [],
+	providers: []
 })
 export class AppModule {}
