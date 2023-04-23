0b04c1601dd242a53f43801839874dc9d42f028e
Elijah Molloy <elijahmolloy@gmail.com>
2023-04-23 01:56:51 -0600

    creation of tokens module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TokensModule } from './tokens/tokens.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
 		MongooseModule.forRoot(process.env.MONGODB_URL),
 		AuthModule,
-		UsersModule
+		UsersModule,
+		TokensModule
 	],
 	controllers: [],
 	providers: []
